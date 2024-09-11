39b7f68541c098566957227c30c79b54c7cd3716
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-09-12 01:08:41 +0400

    add globals styles and fonts

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,111 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle` 
+
+  :root {
+    /* Colors */
+    --clr-green: #45A849;
+    --clr-red: #F93B1D
+    --clr-blue-grey:#2D3648;
+    --clr-white: #ffffff;
+    --clr-dark: #021526;
+
+    --clr-bg-primary: #ffffff;
+
+    --clr-text-primary: #021526;
+    --clr-text-secondary: #808A93;
+    --clr-text-secondary-dark: #676E76;
+
+    --clr-error: #F93B1D
+    --clr-success: #45A849;
+    --clr-btn-primary:#F93B1D;
+    --clr-btn-primary-hover: #DF3014;
+    --clr-btn-secondary:#808A93;
+    --clr-btn-primary-hover: #DF3014;
+
+
+    /* Font Sizes */
+    --fs-xl: 4.8rem;
+    --fs-lg: 3.2rem;
+    --fs-medium: 2.4rem;
+    --fs-small: 1.6rem;
+    --fs-xs: 1.4rem;
+  
+    --fs-body: 1.6rem;
+    --fs-button: 1.6rem;
+    --fs-subtitle: 1.6rem;
+    --fs-label: 1.4rem;
+    --fs-input: 1.4rem;
+    --fs-hint: 1.4rem;
+
+    /* Borders */
+    --border: 1px solid #DBDBDB; 
+  }
+
+
+  *,
+  *::before,
+  *::after { 
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+  } 
+
+  html {
+    font-size: 62.5%; 
+  }
+
+
+  @font-face {
+  font-family: "FiraGO";
+  src: url("/fonts/FiraGO-Regular.otf") format("opentype");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "FiraGO";
+  src: url("/fonts/FiraGO-Medium.woff") format("opentype");
+  font-weight: 500;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "FiraGO";
+  src: url("/fonts/FiraGO-Bold.woff2") format("opentype");
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+}
+  
+  ul, ol{
+    list-style-type: none;
+  }
+
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden; 
+  }
+
+
+  body {
+    font-family: 'FiraGO', sans-serif;
+    background-color:var(--clr-bg-primary); 
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    -webkit-text-size-adjust: 100%;
+    -ms-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+  } 
+
+  #root {
+    height: 100%;
+  }
+
+  input, select, textarea, button{font-family:inherit;}
+`;
